a701703cefe27a316affa0b47000b6d6f2fb50ba
Nithya AJ <aj7nithya@gmail.com>
2023-05-06 13:41:29 +0530

    displaying data in frontend

--- client/src/pages/Widgets/TabWidget.jsx
+++ client/src/pages/Widgets/TabWidget.jsx
@@ -9,7 +9,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import IconCmp from 'Components/IconCmp';
 import { Button, ButtonGroup, Divider } from '@mui/material';
-import { useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 function TabPanel(props) {
@@ -45,35 +45,41 @@ function a11yProps(index) {
     };
 }
 
-
 export default function TabWidget() {
-
-
-    const [data, setData] = React.useState([])
 
     const theme = useTheme();
     const [value, setValue] = React.useState(0);
+    const [categories, setCategories] = useState([])
+    const [dishList, setDishList] = useState([])
+
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
+        console.log(newValue);
+        setDishList(categories[newValue].category_dishes)
     };
 
     const handleChangeIndex = (index) => {
+        console.log('vanneeee');
         setValue(index);
+        setDishList(categories[index].category_dishes)
+        console.log(index);
     };
 
-    const DataUrl = 'https://run.mocky.io/v3/a67edc87-49c7-4822-9cb4-e2ef94cb3099'
+    React.useEffect(() => {
+        axios
+            .get("https://run.mocky.io/v3/a67edc87-49c7-4822-9cb4-e2ef94cb3099")
+            .then((response) => {
+                const data = response.data[0]
+                setCategories(data.table_menu_list)
+                // setDishList(data.table_menu_list.map((category) => category.category_dishes))
+                setDishList(data.table_menu_list[0].category_dishes)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [categories, dishList]);
 
-    const fetchData = async () => {
-        const res = await axios.get(DataUrl)
-            .catch(err => console.log(err))
-        const data = await res.data
-        console.log(data);
-        return data
-    }
-    useEffect(() => {
-        fetchData().then(data => setData(data))
-    }, [])
 
     return (
         <Box sx={{ bgcolor: 'background.paper', width: '100%' }}>
@@ -87,15 +93,7 @@ export default function TabWidget() {
                     aria-label="full width tabs example"
                     visibleScrollbar={true}
                 >
-                    <Tab label="Item One" {...a11yProps(0)} sx={{ width: '23rem' }} />
-                    <Tab label="Item Two" {...a11yProps(1)} sx={{ width: '23rem' }} />
-                    <Tab label="Item Three" {...a11yProps(3)} sx={{ width: '23rem' }} />
-                    <Tab label="Item Three" {...a11yProps(4)} sx={{ width: '23rem' }} />
-                    <Tab label="Item Three" {...a11yProps(5)} sx={{ width: '23rem' }} />
-                    <Tab label="Item Three" {...a11yProps(6)} sx={{ width: '23rem' }} />
-                    <Tab label="Item Three" {...a11yProps(2)} sx={{ width: '23rem' }} />
-                    <Tab label="Item Three" {...a11yProps(2)} sx={{ width: '23rem' }} />
-                    <Tab label="Item Three" {...a11yProps(2)} sx={{ width: '23rem' }} />
+                    {categories.map((category, index) => (<Tab label={category.menu_category} key={category.menu_category_id} {...a11yProps(index)} sx={{ width: '23rem' }} />))}
                 </Tabs>
             </AppBar>
             <SwipeableViews
@@ -103,112 +101,50 @@ export default function TabWidget() {
                 index={value}
                 onChangeIndex={handleChangeIndex}
             >
-                <TabPanel value={value} index={0} dir={theme.direction}>
-                    <Box container spacing={2} sx={{ px: '1rem', flexGrow: 1, display: 'flex' }}>
-                        <Box gap={2} item xs={9.5} sx={{ display: 'flex', flexGrow: 4 }}>
-                            <Box>
-                                <IconCmp color={'green'} />
-                            </Box>
-                            <Box>
-                                <Typography sx={{ fontWeight: '500' }}>Spinach Salad</Typography>
-                                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginRight: "1rem" }}>
-                                    <Typography sx={{ fontSize: '14px', fontWeight: '500' }}>SAR 7.95</Typography>
-                                    <Typography sx={{ display: { xs: 'block', sm: 'block', md: 'none' } }}>15 Calories</Typography>
+                {categories.map((category, index) => (
+                    <TabPanel key={category.menu_category_id} value={value} index={index} dir={theme.direction}>
+                        {dishList.map((dish, index) => (
+                            <Box key={dish.dish_id} container spacing={2} sx={{ px: '1rem', flexGrow: 1, display: 'flex' }}>
+                                <Box gap={2} item xs={9.5} sx={{ display: 'flex', flexGrow: 4 }}>
+                                    {dish.dish_Type === 1 ?
+                                        (<Box>
+                                            <IconCmp color={'red'} />
+                                        </Box>) :
+                                        (<Box>
+                                            <IconCmp color={'green'} />
+                                        </Box>)
+                                    }
+                                    <Box>
+                                        <Typography sx={{ fontWeight: '500' }}>{dish.dish_name}</Typography>
+                                        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginRight: "1rem" }}>
+                                            <Typography sx={{ fontSize: '14px', fontWeight: '500' }}>{dish.dish_currency} {dish.dish_price}</Typography>
+                                            <Typography sx={{ display: { xs: 'block', sm: 'block', md: 'none' } }}>{dish.dish_calories} Calories</Typography>
 
+                                        </Box>
+                                        <Typography color='GrayText' variant='subtitle2'>{dish.description}</Typography>
+                                        <Box sx={{ my: '8px' }}>
+                                            <ButtonGroup variant="contained" aria-label="outlined primary button group" sx={{ borderRadius: '20px' }}>
+                                                <Button sx={{ backgroundColor: 'green', borderRadius: '20px', width: '2.7rem', height: '2rem' }}>-</Button>
+                                                <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '2.7rem', backgroundColor: 'green', color: 'white' }}><Typography>0</Typography></Box>
+                                                <Button sx={{ backgroundColor: 'green', borderRadius: '20px', width: '2.7rem', height: '2rem' }}>+</Button>
+                                            </ButtonGroup>
+                                        </Box>
+                                        {dish.addonCat?.length > 0 && (<Typography color='error' variant='subtitle2'>Customization available</Typography>)}
+                                    </Box>
                                 </Box>
-                                <Typography color='GrayText' variant='subtitle2'>Fresh spinach, mushrooms, and hard-boiled egg served with warm abacon vinaigrette</Typography>
-                                <Box sx={{ my: '8px' }}>
-                                    <ButtonGroup variant="contained" aria-label="outlined primary button group" sx={{ borderRadius: '20px' }}>
-                                        <Button sx={{ backgroundColor: 'green', borderRadius: '20px', width: '2.7rem', height: '2rem' }}>-</Button>
-                                        <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '2.7rem', backgroundColor: 'green', color: 'white' }}><Typography>0</Typography></Box>
-                                        <Button sx={{ backgroundColor: 'green', borderRadius: '20px', width: '2.7rem', height: '2rem' }}>+</Button>
-                                    </ButtonGroup>
+                                <Box item xs={1} sx={{ display: { xs: 'none', sm: 'none', md: 'flex' }, alignItems: 'center', justifyContent: 'center ', flexGrow: 1 }}>
+                                    <Typography>{dish.dish_calories} Calories</Typography>
                                 </Box>
-                                <Typography color='error' variant='subtitle2'>Customization available</Typography>
-                            </Box>
-                        </Box>
-                        <Box item xs={1} sx={{ display: { xs: 'none', sm: 'none', md: 'flex' }, alignItems: 'center', justifyContent: 'center ', flexGrow: 1 }}>
-                            <Typography>15 Calories</Typography>
-                        </Box>
-                        <Box item xs={1.5} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-                            <Box>
-                                <img src="https://picsum.photos/seed/picsum/200/300" height='100' width='100' alt="" style={{ borderRadius: "12px" }} />
-                            </Box>
-                        </Box>
-                    </Box>
-
-
-
-                    <Box container spacing={2} sx={{ px: '1rem', flexGrow: 1, display: 'flex' }}>
-                        <Box gap={2} item xs={9.5} sx={{ display: 'flex', flexGrow: 4 }}>
-                            <Box>
-                                <IconCmp color={'green'} />
-                            </Box>
-                            <Box>
-                                <Typography sx={{ fontWeight: '500' }}>Spinach Salad</Typography>
-                                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginRight: "1rem" }}>
-                                    <Typography sx={{ fontSize: '14px', fontWeight: '500' }}>SAR 7.95</Typography>
-                                    <Typography sx={{ display: { xs: 'block', sm: 'block', md: 'none' } }}>15 Calories</Typography>
-
+                                <Box item xs={1.5} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+                                    <Box>
+                                        <img src={dish.dish_image} height='100' width='100' alt="" style={{ borderRadius: "12px" }} />
+                                    </Box>
                                 </Box>
-                                <Typography color='GrayText' variant='subtitle2'>Fresh spinach, mushrooms, and hard-boiled egg served with warm abacon vinaigrette</Typography>
-                                <Box sx={{ my: '8px' }}>
-                                    <ButtonGroup variant="contained" aria-label="outlined primary button group" sx={{ borderRadius: '20px' }}>
-                                        <Button sx={{ backgroundColor: 'green', borderRadius: '20px', width: '2.7rem', height: '2rem' }}>-</Button>
-                                        <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '2.7rem', backgroundColor: 'green', color: 'white' }}><Typography>0</Typography></Box>
-                                        <Button sx={{ backgroundColor: 'green', borderRadius: '20px', width: '2.7rem', height: '2rem' }}>+</Button>
-                                    </ButtonGroup>
-                                </Box>
-                                <Typography color='error' variant='subtitle2'>Customization available</Typography>
+                                <Divider sx={{ mx: '1rem' }}></Divider>
                             </Box>
-                        </Box>
-                        <Box item xs={1} sx={{ display: { xs: 'none', sm: 'none', md: 'flex' }, alignItems: 'center', justifyContent: 'center ', flexGrow: 1 }}>
-                            <Typography>15 Calories</Typography>
-                        </Box>
-                        <Box item xs={1.5} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-                            <Box>
-                                <img src="https://picsum.photos/seed/picsum/200/300" height='100' width='100' alt="" style={{ borderRadius: "12px" }} />
-                            </Box>
-                        </Box>
-                    </Box>
-
-
-
-                    <Box container spacing={2} sx={{ px: '1rem', flexGrow: 1, display: 'flex' }}>
-                        <Box gap={2} item xs={9.5} sx={{ display: 'flex', flexGrow: 4 }}>
-                            <Box>
-                                <IconCmp color={'green'} />
-                            </Box>
-                            <Box>
-                                <Typography sx={{ fontWeight: '500' }}>Spinach Salad</Typography>
-                                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginRight: "1rem" }}>
-                                    <Typography sx={{ fontSize: '14px', fontWeight: '500' }}>SAR 7.95</Typography>
-                                    <Typography sx={{ display: { xs: 'block', sm: 'block', md: 'none' } }}>15 Calories</Typography>
-
-                                </Box>
-                                <Typography color='GrayText' variant='subtitle2'>Fresh spinach, mushrooms, and hard-boiled egg served with warm abacon vinaigrette</Typography>
-                                <Box sx={{ my: '8px' }}>
-                                    <ButtonGroup variant="contained" aria-label="outlined primary button group" sx={{ borderRadius: '20px' }}>
-                                        <Button sx={{ backgroundColor: 'green', borderRadius: '20px', width: '2.7rem', height: '2rem' }}>-</Button>
-                                        <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '2.7rem', backgroundColor: 'green', color: 'white' }}><Typography>0</Typography></Box>
-                                        <Button sx={{ backgroundColor: 'green', borderRadius: '20px', width: '2.7rem', height: '2rem' }}>+</Button>
-                                    </ButtonGroup>
-                                </Box>
-                                <Typography color='error' variant='subtitle2'>Customization available</Typography>
-                            </Box>
-                        </Box>
-                        <Box item xs={1} sx={{ display: { xs: 'none', sm: 'none', md: 'flex' }, alignItems: 'center', justifyContent: 'center ', flexGrow: 1 }}>
-                            <Typography>15 Calories</Typography>
-                        </Box>
-                        <Box item xs={1.5} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-                            <Box>
-                                <img src="https://picsum.photos/seed/picsum/200/300" height='100' width='100' alt="" style={{ borderRadius: "12px" }} />
-                            </Box>
-                        </Box>
-                    </Box>
-                    <Divider sx={{ mx: '1rem' }}></Divider>
-                </TabPanel>
-
+                        ))}
+                    </TabPanel>
+                ))}
             </SwipeableViews>
         </Box >
     );
